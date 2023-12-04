e4c62bf637f6f2bea802e5cb32b7f4268ae80ef8
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2023-12-04 14:28:05 +0200

    Add files via upload

---
+++ backend/config/express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Add more middleware as needed
+
+module.exports = app;
+
